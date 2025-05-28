22ef4b8343c5d0ae3962abc0680a4736c4ef070a
servanaApp <servana.app@gmail.com>
2025-05-28 17:14:42 +0800

    department backend added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 
 const ServanaRouter = require('./routes/profile.js')
+const departmentRoutes = require('./routes/department');   
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,6 +16,7 @@ app.use(cors({origin: true, credentials: true})); // Allow CORS for all origins
 
 app.use(express.json());
 app.use('/profile', ServanaRouter);
+app.use('/departments', departmentRoutes);
 app.listen(port, () => {
     console.log(`Server is running: ${port}`);
 });
